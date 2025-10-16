1698cec1c3055272c5b8ee849e909c18fdefa152
agamlatiff <agam.latiff@gmail.com>
2025-10-16 22:08:17 +0700

    Merchant product service controller

--- saturdaybackend/app/Http/Controllers/MerchantProductController.php
+++ saturdaybackend/app/Http/Controllers/MerchantProductController.php
@@ -3,9 +3,63 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\MerchantProductRequest;
+use App\Http\Requests\MerchantProductUpdateRequest;
+use App\Services\MerchantProductService;
+
 use Illuminate\Http\Request;
 
 class MerchantProductController extends Controller
 {
-    //
+    //    Declare dependency 
+
+    private MerchantProductService $merchantProductService;
+    public function __construct(MerchantProductService $merchantProductService)
+    {
+        $this->merchantProductService = $merchantProductService;
+    }
+
+    public function store(MerchantProductRequest $request, int $merchant)
+    {
+        // Validated data from input data 
+        $validated = $request->validated();
+        $validated["merchant_id"] = $merchant;
+
+        // Validation from service 
+        $merchantProduct = $this->merchantProductService->assignProductToMerchant($validated);
+
+
+        // If valid, return data as JSON
+        return response()->json([
+            "message" => "Product assigned to merchant successfully",
+            "data" => $merchantProduct
+        ], 201);
+    }
+
+    public function update(MerchantProductUpdateRequest $request, int $merchantId, $productId)
+    {
+        // Validation data from input
+        $validated = $request->validated();
+
+        // Validation logic from service
+        $merchantProduct = $this->merchantProductService->updateStock($merchantId, $productId, $validated["stock"], $validated["warehouse_id"]);
+
+        // If valid, return data as JSON
+        return response()->json([
+            "message" => "Stock updated successfully",
+            "data" => $merchantProduct
+        ]);
+    }
+
+    public function destroy(int $merchant, int $product)
+    {
+        // Remove data and validation login from service
+        $this->merchantProductService->removeProductFromMerchant($merchant, $product);
+
+        
+        // If valid, return message as JSON
+        return response()->json([
+            "message" => "Product detached from merchant successfully"
+        ]);
+    }
 }
--- saturdaybackend/app/Services/MerchantProductService.php
+++ saturdaybackend/app/Services/MerchantProductService.php
@@ -2,7 +2,9 @@
 
 namespace App\Services;
 
+use App\Models\Merchant;
 use App\Repositories\MerchantProductRepository;
+use App\Repositories\MerchantRepository;
 use App\Repositories\WarehouseProductRepository;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
@@ -13,9 +15,11 @@ class MerchantProductService
   // Assign data from repository
   private MerchantProductRepository $merchantProductRepository;
   private WarehouseProductRepository $warehouseProductRepository;
+  private MerchantRepository $merchantRepository;
 
-  public function __construct(MerchantProductRepository $merchantProductRepository, WarehouseProductRepository $warehouseProductRepository)
+  public function __construct(MerchantProductRepository $merchantProductRepository, WarehouseProductRepository $warehouseProductRepository, merchantRepository $merchantRepository)
   {
+    $this->merchantRepository = $merchantRepository;
     $this->merchantProductRepository = $merchantProductRepository;
     $this->warehouseProductRepository = $warehouseProductRepository;
   }
@@ -53,6 +57,104 @@ class MerchantProductService
       );
 
     });
+  }
+
+  public function updateStock(int $merchantId, int $productId, int $newStock, int $warehouseId)
+  {
+    return DB::transaction(function () use ($merchantId, $productId, $newStock, $warehouseId) {
+
+      // Checking data in database
+      $existing = $this->merchantProductRepository->getByMerchantAndProduct($merchantId, $productId);
+
+      // Validation data 
+      if (!$existing) {
+        throw ValidationException::withMessages([
+          "product" => ["Product not assigned to this merchant."]
+        ]);
+      }
+
+      if (!$warehouseId) {
+        throw ValidationException::withMessages([
+          "warehouse_id" => ["Warehouse ID is required when increasing stock."]
+        ]);
+      }
+
+      // Current stock in merchant
+      $currentStock = $existing->stock;
+
+      // Validation when new stock greatest then current stock
+      if ($newStock > $currentStock) {
+        $diff = $newStock - $currentStock;
+
+        // Checking product
+        $warehouseProduct = $this->warehouseProductRepository->getByWarehouseAndProduct($warehouseId, $productId);
+
+        // Validation data
+        if (!$warehouseProduct || $warehouseProduct->stock < $diff) {
+          throw ValidationException::withMessages([
+            "Stock" => ["Insufficient stock in warehouse"]
+          ]);
+        }
+
+        // Subtraction stock
+        $this->warehouseProductRepository->updateStock(
+          $warehouseId,
+          $productId,
+          $warehouseProduct->stock - $diff
+        );
+
+      }
+
+      // Validation when new stock lowest then current stock
+      if ($newStock < $currentStock) {
+        $diff = $currentStock - $newStock;
+
+        // Checking product
+        $warehouseProduct = $this->warehouseProductRepository->getByWarehouseAndProduct($warehouseId, $productId);
+
+        // Validation data product
+        if (!$warehouseProduct) {
+          throw ValidationException::withMessages([
+            "warehouse" => ["Product not found in warehouse."]
+          ]);
+        }
+
+        // Update stock 
+        $this->warehouseProductRepository->updateStock($warehouseId, $productId, $warehouseProduct->stock - $diff);
+      }
+
+
+      return $this->merchantProductRepository->updateStock($warehouseId, $productId, $newStock);
+
+    });
+  }
+
+  public function removeProductFromMerchant(int $merchantId, int $productId)
+  {
+    // $merchant = Merchant::findOrFail($merchantId);
+
+    // Get one data from database
+    $merchant = $this->merchantRepository->getById($merchantId, $fields ?? ["*"]);
+
+    //  Checking if merchant not exists
+    if (!$merchant) {
+      throw ValidationException::withMessages([
+        "merchant" => ["Merchant is not found."]
+      ]);
+    }
+
+    // Get data merchant and product from database
+    $exists = $this->merchantProductRepository->getByMerchantAndProduct($merchantId, $productId);
+
+    //  Checking if data not exists
+    if (!$exists) {
+      throw ValidationException::withMessages([
+        "product" => ["Product not assigned to this merchant."]
+      ]);
+    }
+
+    $merchant->products()->detach($productId);
+
   }
 
 }
